a5ce8a64a83b1eb7aaed1401b98a9f57793b4f96
whasun <suuneeee@naver.com>
2024-07-09 12:14:29 +0900

    쇼핑

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Root from "./page/Root";
+import ProductAll from "./page/ProductAll";
+import Login from "./page/Login";
+import ProductDetail from "./page/ProductDetail";
+import PrivateRoute from "./route/PrivateRoute";
+
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [authenticate, setAuthenticate] = useState(false);
+  useEffect(() => {
+    console.log("로그인");
+  }, [authenticate]);
+
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: (
+        <Root authenticate={authenticate} setAuthenticate={setAuthenticate} />
+      ),
+      children: [
+        { index: true, element: <ProductAll /> },
+        {
+          path: "/login",
+          element: <Login setAuthenticate={setAuthenticate} />,
+        },
+        {
+          path: "/product/:id",
+          element: <PrivateRoute authenticate={authenticate} />,
+        },
+      ],
+    },
+  ]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <RouterProvider router={router}>
+      <p>App</p>
+    </RouterProvider>
   );
 }
 
